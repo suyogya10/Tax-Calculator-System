d15baf58cacc2e7bd39bd6876030b2c4aff3da29
Ashim0429 <reachashim@gmail.com>
2022-04-06 13:14:49 +0545

    admin login uploaded

---
+++ admin/config.php
@@ -0,0 +1,14 @@
+<?php
+$dbname = "tax_news";
+    $hostname = "localhost";
+    $username = "root";
+    $dbpassword = "";
+
+    $con = new mysqli("$hostname", "$username", "$dbpassword", "$dbname");
+    // Check connection
+    if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+  }
+  echo "Connected successfully";
+
+  ?>
---
+++ admin/register.php
@@ -0,0 +1,145 @@
+<?php
+    require "./config.php";
+  
+
+    session_start();
+    if(isset($_SESSION['isloggedin'])){
+        header('Location: ./index.php');
+
+    }
+    if(isset($_POST['register'])){
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $email = $_POST['email'];
+        $pass = md5($_POST['pass']);
+        $cpass = md5($_POST['cpass']);
+        if($pass == $cpass){
+            $checkuser = mysqli_query($con, "SELECT * FROM user_login WHERE email='$email'");
+            $countuser = mysqli_num_rows($checkuser);
+            if($countuser == 1){
+                $error = "<p>User with this email already exists.</p>";
+            }
+            else{
+                $query =  "INSERT INTO user_login (fname, lname, email, pass) VALUES('$fname', '$lname', '$email', '$pass')";
+                if(mysqli_query($con, $query)){
+                    $success = "User Registered Successfully. Click <a href='./login.php'> here to login.</a>";
+                }
+
+                
+
+                   
+                
+            }
+           
+            
+        }
+        else{
+            $error = "<p>Please Enter the same password.</p>";
+            
+        }
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Register - SB Admin</title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-7">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Create Account</h3></div>
+                                    <div class="card-body">
+                                        <form method="post" action="register.php">
+                                            <?php
+                                            if(!empty($error)):
+
+                                            echo"<div class='alert alert-danger' role='alert'>"
+                                            . $error .
+                                        "</div>";
+                                            endif;
+                                            if(!empty($success)):
+
+                                                echo"<div class='alert alert-success' role='alert'>"
+                                                . $success .
+                                            "</div>";
+                                                endif;
+                                        ?>
+                                            <div class="row mb-3">
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" id="inputFirstName" type="text" name="fname" placeholder="Enter your first name" />
+                                                        <label for="inputFirstName">First name</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-floating">
+                                                        <input class="form-control" id="inputLastName" type="text" name="lname" placeholder="Enter your last name" />
+                                                        <label for="inputLastName">Last name</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputEmail" type="email" name="email" placeholder="name@example.com" />
+                                                <label for="inputEmail">Email address</label>
+                                            </div>
+                                            <div class="row mb-3">
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" id="inputPassword" type="password" name="pass" placeholder="Create a password" />
+                                                        <label for="inputPassword">Password</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-floating mb-3 mb-md-0">
+                                                        <input class="form-control" id="inputPasswordConfirm" type="password" name="cpass" placeholder="Confirm password" />
+                                                        <label for="inputPasswordConfirm">Confirm Password</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="mt-4 mb-0">
+                                                <div class="d-grid"><button class="btn btn-primary btn-block" name="register" type="submit">Create Account</button></div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="card-footer text-center py-3">
+                                        <div class="small"><a href="login.html">Have an account? Go to login</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2021</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
